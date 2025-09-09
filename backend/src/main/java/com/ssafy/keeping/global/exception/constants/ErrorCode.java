f023a1581b21d6f871d1e9319d97c221d420bd6b
정한슬 <hschung135@naver.com>
2025-09-09 14:20:17 +0900

    Fix: error enum message ì수정

--- backend/src/main/java/com/ssafy/keeping/global/exception/constants/ErrorCode.java
+++ backend/src/main/java/com/ssafy/keeping/global/exception/constants/ErrorCode.java
@@ -10,7 +10,7 @@ public enum ErrorCode {
     // Store 관련
     STORE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 매장을 찾을 수 없습니다."),
     STORE_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 등록한 주소의 가게입니다. 다시 확인해주세요."),
-    STORE_INVALID(HttpStatus.BAD_REQUEST, "해당 매장을 찾을 수 없습니다.")
+    STORE_INVALID(HttpStatus.BAD_REQUEST, "해당 매장의 운영상태를 확인해주세요.")
     ;
     private final HttpStatus httpStatus;
     private final String message;
