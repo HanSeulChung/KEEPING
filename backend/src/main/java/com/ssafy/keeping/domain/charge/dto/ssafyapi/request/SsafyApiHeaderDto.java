923a1973c30acd00e18c7c855a8e1ac6905fcf12
김영준 <bill5500@naver.com>
2025-09-11 13:26:30 +0900

    feat(dto): 카드결제와 관련한 dto 설정 완료

---
+++ backend/src/main/java/com/ssafy/keeping/domain/charge/dto/ssafyapi/request/SsafyApiHeaderDto.java
@@ -0,0 +1,40 @@
+package com.ssafy.keeping.domain.charge.dto.ssafyapi.request;
+
+import lombok.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class SsafyApiHeaderDto {
+
+    private String apiName;
+    private String transmissionDate;
+    private String transmissionTime;
+    private String institutionCode;
+    private String fintechAppNo;
+    private String apiServiceCode;
+    private String institutionTransactionUniqueNo;
+    private String apiKey;
+    private String userKey;
+
+    public static SsafyApiHeaderDto createCardPaymentHeader(
+            String transmissionDate,
+            String transmissionTime,
+            String institutionTransactionUniqueNo,
+            String apiKey,
+            String userKey) {
+        
+        return SsafyApiHeaderDto.builder()
+                .apiName("createCreditCardTransaction")
+                .transmissionDate(transmissionDate)
+                .transmissionTime(transmissionTime)
+                .institutionCode("00100")
+                .fintechAppNo("001")
+                .apiServiceCode("createCreditCardTransaction")
+                .institutionTransactionUniqueNo(institutionTransactionUniqueNo)
+                .apiKey(apiKey)
+                .userKey(userKey)
+                .build();
+    }
+}
