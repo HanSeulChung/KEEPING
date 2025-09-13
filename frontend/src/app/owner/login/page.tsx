8ef582e91f6c25ddb7d0ace9448ca2a1900b3bb8
yy <juyy99@naver.com>
2025-09-13 22:21:27 +0900

    feat : 사장님 로그인 페이지 UI

---
+++ frontend/src/app/owner/login/page.tsx
@@ -0,0 +1,5 @@
+import OwnerLogin from "@/components/owner/OwnerLogin";
+
+export default function Page() {
+  return <OwnerLogin />;
+}
--- frontend/src/components/common/Header.tsx
+++ frontend/src/components/common/Header.tsx
@@ -6,27 +6,42 @@ export default function Header() {
         <div className="w-full bg-white pt-2 sm:pt-3 md:pt-4">
             {/* 상단 검은색 가로선 */}
             <div className="w-full border-t border-border"></div>
-            {/* 헤더 */}
-            <header className="w-full h-12 sm:h-14 md:h-16 lg:h-18 flex items-center justify-between border-b border-l border-r border-border bg-white px-3 sm:px-4 md:px-6">
-                {/* 뒤로가기 버튼 */}
-                <button className="p-1 sm:p-2 hover:bg-gray-100 rounded" aria-label="뒤로가기">
-                    <Image src="/back.svg" alt="뒤로가기" width={16} height={16} className="sm:w-5 sm:h-5 md:w-6 md:h-6" />
-                </button>
 
-                {/* 세로 구분선 */}
-                <div className="h-12 sm:h-14 md:h-16 lg:h-18 border-l border-border"></div>
+            {/* 헤더 */}
+            <header className="w-full h-12 sm:h-14 md:h-16 flex justify-between border-b border-l border-r border-border bg-white px-4 sm:px-6">
+
+                {/* Left: 뒤로가기 버튼 + 구분선 */}
+                <div className="flex items-center">
+                    <button className="p-2 sm:p-3 hover:bg-gray-100 rounded" aria-label="뒤로가기">
+                        <Image
+                            src="/back.svg"
+                            alt="뒤로가기"
+                            width={20}
+                            height={20}
+                            className="sm:w-6 sm:h-6 md:w-7 md:h-7"
+                        />
+                    </button>
+                    {/* 세로 구분선 */}
+                    <div className="self-stretch border-l border-border mx-3"></div>
+                </div>
 
                 {/* Center: Logo */}
-                <span className="font-extrabold text-sm sm:text-base md:text-lg lg:text-xl text-text font-display">KEEPING</span>
+                <div className="flex items-center">
+                    <span className="font-extrabold text-base sm:text-lg md:text-xl text-text font-display">
+                        KEEPING
+                    </span>
+                </div>
 
-                {/* 세로 구분선 */}
-                <div className="h-12 sm:h-14 md:h-16 lg:h-18 border-l border-border"></div>
-
-                {/* Right: Actions */}
-                <div className="flex items-center space-x-1 sm:space-x-2">
-                    <button className="p-1 sm:p-2 rounded hover:bg-gray-100" aria-label="알림">
-                        <Image src="/bell.svg" alt="알림" width={48} height={48} className="w-16 h-16 sm:w-12 sm:h-12 md:w-14 md:h-14" />
-                    </button>
+                {/* Right: 구분선 + 알림 버튼 */}
+                <div className="flex items-center">
+                    <div className="self-stretch border-l border-border mx-3"></div>
+                        <Image
+                            src="/bell.svg"
+                            alt="알림"
+                            width={48}
+                            height={48}
+                            className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12"
+                        />
                 </div>
             </header>
         </div>
