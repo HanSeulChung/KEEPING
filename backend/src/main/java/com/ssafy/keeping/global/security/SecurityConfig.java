4430df79e4e5d71b339b96ef767e52b249c8a023
김영준 <bill5500@naver.com>
2025-09-18 22:04:37 +0900

    fix(charge): 보너스 포인트에 따른 결제와 결제취소 처리

--- backend/src/main/java/com/ssafy/keeping/global/security/SecurityConfig.java
+++ backend/src/main/java/com/ssafy/keeping/global/security/SecurityConfig.java
@@ -43,14 +43,18 @@ public class SecurityConfig {
             "/swagger-resources/**",
             "/favicon.ico",
             "/.well-known/**",
-            "/s3/**"
+            "/.well-known/**",
+            "/owners/stores/**",
+            "/api/v1/stores/**"
     };
 
     public static final String[] TEMP_ALLOW_URLS = {
             "/stores/**",
             "/api/**",
             "/api/v1/stores/**",
-            "/wallets/**"
+            "/wallets/**",
+            "/owners/*/stores/*/charge-bonus",
+            "/owners/*/stores/*/charge-bonus/*"
     };
 
     private final ClientRegistrationRepository clientRegistrationRepository;
