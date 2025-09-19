89b098cc1b4b48a78fb6879858c842a3fe99fdbd
haeuni <hyeun9313@gmail.com>
2025-09-19 16:18:14 +0900

    fix: 파일입력

--- backend/src/main/java/com/ssafy/keeping/domain/user/owner/controller/OwnerController.java
+++ backend/src/main/java/com/ssafy/keeping/domain/user/owner/controller/OwnerController.java
@@ -6,6 +6,7 @@ import com.ssafy.keeping.domain.user.owner.service.OwnerService;
 import com.ssafy.keeping.global.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,7 +19,7 @@ public class OwnerController {
     private final OwnerService ownerService;
 
     // 프로필 이미지 수정
-    @PostMapping("/{ownerId}/profile-image/upload")
+    @PostMapping(value = "/{ownerId}/profile-image/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<ApiResponse<ProfileUploadResponse>> uploadProfileImage(@PathVariable Long ownerId,
                                                                                  @RequestParam("file") MultipartFile file) {
         ProfileUploadResponse response = ownerService.uploadProfileImage(ownerId, file);
